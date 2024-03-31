92fb99b688a003877c14ea51b42223af9dcc97c5
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-03-31 23:49:27 +0330

    chore: add new story for the button + implement chromatic

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -35,6 +35,13 @@ export const PrimaryLarge: Story = {
   },
 };
 
+export const PrimaryExtraSmall: Story = {
+  args: {
+    size: "xs",
+    variant: "primary",
+  },
+};
+
 export const SecondaryLarge: Story = {
   args: {
     size: "lg",
