ebc9ce60dddb06f6e4c0d1b8c8e79cf3ac3c251a
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-07-18 00:08:12 +0330

    feat: implement the input component

---
+++ lib/components/input.tsx
@@ -0,0 +1,39 @@
+import { cx } from "class-variance-authority";
+import React, { InputHTMLAttributes } from "react";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  helpText?: string;
+  error?: string;
+}
+
+const Input = ({ label, id, className, helpText, error, ...props }: Props) => {
+  return (
+    <div>
+      {label && (
+        <label className="text-sm font-medium text-gray-dark" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <div
+        className={cx(
+          "border border-gray rounded-md h-9 mt-1 overflow-hidden focus-within:border-primary transition-colors",
+          error ? "border-red-light" : "",
+        )}
+      >
+        <input
+          id={id}
+          className={cx(
+            "px-3 text-sm text-gray-dark outline-none h-full",
+            className ?? "",
+          )}
+          {...props}
+        />
+      </div>
+      {helpText && <p className="mt-2 text-sm text-gray-text">{helpText}</p>}
+      {error && <p className="mt-2 text-sm text-red-text">{error}</p>}
+    </div>
+  );
+};
+
+export default Input;
---
+++ lib/stories/input.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Input } from "../main.ts";
+import "../output.css";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  title: "Input",
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    className: "w-96",
+    placeholder: "you@example.com",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const InputWithLabelAndHelpText: Story = {
+  args: {
+    label: "Email",
+    helpText: "We'll only use this for spam.",
+  },
+};
+
+export const InputWithLabelAndValidationError: Story = {
+  args: {
+    label: "Email",
+    error: "We'll only use this for spam.",
+  },
+};
+
+export const InputWithHiddenLabel: Story = {
+  args: {},
+};
+
+export const InputWithDisabledState: Story = {
+  args: {
+    disabled: true,
+  },
+};
